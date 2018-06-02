086a4ef215c8fa00f0ccf1d91880f840ce404a36
redjen8 <seokwoo2525@gmail.com>
2018-06-03 03:03:10 +0900

    Board some element missed, added

--- chess/Board_2.java
+++ chess/Board_2.java
@@ -9,34 +9,39 @@ import pieces.Position;
 import pieces.Queen;
 import pieces.Rook;
 
-public class Board_2 implements ConstDef{
+public class Board_2 implements ConstDef {
 	// 20180531 RedJen Initialized
 
 	private Piece[][] board;
-	
+
 	final Piece[][] initialBoard = {
-			{ null, null, null, new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE), new Queen(WHITE), new King(WHITE),
-					new Bishop(WHITE), new Knight(WHITE), new Rook(WHITE), null, null, null},
-			{ null, null ,null, new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
-					new Pawn(WHITE), new Pawn(WHITE), null, null, null},
-			{ null, null, null, null, null, null, null, null, null, null, null, null, null, null}, 
-			{ new Rook(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Rook(GREEN)},
-			{ new Knight(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Knight(GREEN) },
-			{ new Bishop(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Bishop(GREEN) },
-			{ new Queen(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Queen(GREEN) },
-			{ new King(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new King(GREEN) },
-			{ new Bishop(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Bishop(GREEN) },
-			{ new Knight(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Knight(GREEN) },
-			{ new Rook(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Rook(GREEN)},
-			{ new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
-					new Pawn(BLACK), new Pawn(BLACK) },
-			{ new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK), new Queen(BLACK), new King(BLACK),
-					new Bishop(BLACK), new Knight(BLACK), new Rook(BLACK) } };
+		{ null, null, null, new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), null, null, null, },
+		{ null, null, null, new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK), new Queen(BLACK), new King(BLACK), new Bishop(BLACK), new Knight(BLACK), new Rook(BLACK), null, null, null },
+		{ null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+		{ new Rook(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Rook(GREEN)},
+		{ new Knight(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Knight(GREEN) },
+		{ new Bishop(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Bishop(GREEN) },
+		{ new Queen(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Queen(GREEN) },
+		{ new King(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new King(GREEN) },
+		{ new Bishop(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Bishop(GREEN) },
+		{ new Knight(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Knight(GREEN) },
+		{ new Rook(RED), new Pawn(RED), null, null, null, null, null, null, null, null, null, null, new Pawn(GREEN), new Rook(GREEN)},
+		{ null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+		{ null, null ,null, new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), null, null, null},
+		{ null, null, null, new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE), new Queen(WHITE), new King(WHITE), new Bishop(WHITE), new Knight(WHITE), new Rook(WHITE), null, null, null}
+		};
 	
 	public int turn;
+	public Piece curPiece;
+	public Position curPiecePos;
 	public boolean p1_catchable[][] = new boolean[14][14];
 	public boolean p2_catchable[][] = new boolean[14][14];
-
+	
+	public Board_2() {
+		this.board = initialBoard;
+		curPiece = null;
+	}
+	
 	void Move(Position PiecePosition, Position dest) {
 		board[dest.getX()][dest.getY()] = board[PiecePosition.getX()][PiecePosition.getY()];
 		board[PiecePosition.getX()][PiecePosition.getY()] = null;
