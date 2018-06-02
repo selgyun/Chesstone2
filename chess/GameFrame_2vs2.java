af377832744ddcdea46f57ec2af023b6f3bdb0f0
redjen8 <seokwoo2525@gmail.com>
2018-06-03 03:19:04 +0900

    2 2 gameframe initialized

--- chess/GameFrame_2vs2.java
+++ chess/GameFrame_2vs2.java
@@ -1,19 +1,71 @@
 package chess;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameFrame_2vs2 {
-	final int width = 600;
-	final int height = 600;
+	private static final long serialVersionUID = 1L;
+
+	final int width = 840;
+	final int height = 840;
 	JFrame gameFrame;
-	
-	public GameFrame_2vs2(){
+	Board_2 board;
+	JPanel[][] square = new JPanel[14][14];
+	JPanel chessBoard;
+	ImagePanel[][] imgPan = new ImagePanel[14][14];
+
+	public GameFrame_2vs2() {
 		gameFrame = new JFrame("Chess - 2vs2");
 		gameFrame.setSize(width, height);
 		gameFrame.setLocationRelativeTo(null);
-		
+		gameFrame.setLayout(new BorderLayout());
+
+		JPanel chessBoard = new JPanel();
+		chessBoard.setLayout(new GridLayout(14, 14));
+
+		board = new Board_2();
+
+		boolean painter = false;
+		for (int i = 0; i < 14; i++) {
+			for (int j = 0; j < 14; j++) {
+				square[i][j] = new JPanel();
+				square[i][j].putClientProperty("column", i);
+				square[i][j].putClientProperty("row", j);
+				if (painter) {
+					square[i][j].setBackground(Color.ORANGE);
+					painter = false;
+				} else {
+					square[i][j].setBackground(Color.YELLOW);
+					painter = true;
+				}
+				square[i][j].addMouseListener(new MouseEventHandler(board, this));
+				chessBoard.add(square[i][j]);
+			}
+			painter = painter ? false : true;
+		}
+
+		for (int i = 0; i < 14; i++) {
+			for (int j = 0; j < 14; j++) {
+				try {
+					imgPan[i][j] = new ImagePanel();
+					imgPan[i][j].setImage(board.getPiece(i, j).getImg());
+					imgPan[i][j].setPreferredSize(new Dimension(60, 60));
+					square[i][j].add(imgPan[i][j]);
+
+				} catch (NullPointerException err) {
+				}
+			}
+		}
+
+		gameFrame.add(chessBoard, BorderLayout.CENTER);
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.setVisible(true);
+
 	}
-	
+
 }
