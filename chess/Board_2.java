fc75b92cd08c4a42c043ce12ea3c16a17953be29
jung <01048733438a@gmail.com>
2018-06-04 14:39:51 +0900

    board_master update(board1 + board2)

--- chess/Board_2.java
+++ chess/Board_2.java
@@ -4,9 +4,6 @@ import pieces.*;
 
 public class Board_2 extends Board_Master implements ConstDef {
 	// 20180531 RedJen Initialized
-
-	private Piece[][] board;
-
 	final Piece[][] initialBoard = {
 		{ new MT(), new MT(), new MT(), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new MT(), new MT(), new MT(), },
 		{ new MT(), new MT(), new MT(), new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK), new Queen(BLACK), new King(BLACK), new Bishop(BLACK), new Knight(BLACK), new Rook(BLACK), new MT(), new MT(), new MT() },
@@ -24,30 +21,9 @@ public class Board_2 extends Board_Master implements ConstDef {
 		{ new MT(), new MT(), new MT(), new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE), new Queen(WHITE), new King(WHITE), new Bishop(WHITE), new Knight(WHITE), new Rook(WHITE), new MT(), new MT(), new MT()}
 		};
 	
-	public int turn;
-	public Piece curPiece;
-	public Position curPiecePos;
-	public boolean p1_catchable[][] = new boolean[14][14];
-	public boolean p2_catchable[][] = new boolean[14][14];
-	
 	public Board_2() {
 		Piece.players = 2;
-		this.board = initialBoard;
+		board = initialBoard;
 		curPiece = null;
-	}
-	
-	void Move(Position PiecePosition, Position dest) {
-		board[dest.getX()][dest.getY()] = board[PiecePosition.getX()][PiecePosition.getY()];
-		board[PiecePosition.getX()][PiecePosition.getY()] = null;
-	}
-
-	Piece getPiece(int PieceX, int PieceY) {
-		return board[PieceX][PieceY];
-	}
-	
-	void nextTurn() {
-		turn += 1;
-		if (turn > 4)
-			turn = 1;
 	}
 }
--- chess/Board_Master.java
+++ chess/Board_Master.java
@@ -1,10 +1,34 @@
 package chess;
 
-import pieces.*;
+import pieces.Piece;
+import pieces.Position;
 
-public class Board_Master implements ConstDef{
-	public int turn;
+public class Board_Master implements ConstDef {
+	protected int turn;
+	protected int player;
+
+	protected Piece[][] board;
+	public Piece curPiece;
+	public Position curPiecePos;
 	
-	public boolean p1_catchable[][];
-	public boolean p2_catchable[][];
+	public int getTurn() {
+		return turn;
+	}
+
+	public void nextTurn() {
+		turn += player;
+		if (turn > 4)
+			turn = WHITE;
+	}
+	
+
+	void Move(Position PiecePosition, Position dest) {
+		board[dest.getX()][dest.getY()] = board[PiecePosition.getX()][PiecePosition.getY()];
+		board[PiecePosition.getX()][PiecePosition.getY()] = null;
+	}
+
+	Piece getPiece(int PieceX, int PieceY) {
+		return board[PieceX][PieceY];
+	}
+	
 }
