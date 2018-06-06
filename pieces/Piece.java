8148dec5b9aa73c3450c8471f8be3db81b0f7aae
jung <01048733438a@gmail.com>
2018-06-06 19:42:43 +0900

    getMovement 수정

--- pieces/Piece.java
+++ pieces/Piece.java
@@ -1,29 +1,20 @@
 package pieces;
-
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import chess.*;
 
-public abstract class Piece implements ConstDef {
+public abstract class Piece implements ConstDef{
 	protected int color;
-	protected int team; // TEAM 1 : Black, White || TEAM 2 : Red, Green
+	protected int team; //TEAM 1 : Black, White || TEAM 2 : Red, Green
 	protected int name;
 	protected BufferedImage img = null;
 	public static int players = 1;
 	protected static boolean checking = false;
-
-	public int getColor() {
-		return color;
-	}
-
-	public int getName() {
-		return name;
-	}
-
-	public BufferedImage getImg() {
-		return img;
-	}
-
+	
+	public int getColor() { return color; }
+	public int getName() { return name; }
+	public BufferedImage getImg() { return img; }
+	
 	abstract public ArrayList<Position> getMovement(Board_Master board, Position now);
 }
