c37625af73f59fc72ba65c2523164d3ff15b1ca4
jung <01048733438a@gmail.com>
2018-06-07 00:06:32 +0900

    checker

--- chess/Checker.java
+++ chess/Checker.java
@@ -1,16 +1,19 @@
 package chess;
 
+import java.util.ArrayList;
+
+import pieces.Piece;
 import pieces.Position;
 
 public class Checker implements ConstDef {
 
 	// kindkiz ¿€º∫
 	Board_Master board;
-	
+
 	public Checker(Board_Master board) {
 		this.board = board;
 	}
-	
+
 	public boolean isChecked() {
 		// find king position
 		int KingX = 0, KingY = 0;
@@ -23,7 +26,7 @@ public class Checker implements ConstDef {
 				}
 			}
 		}
-		// done
+
 		// is king underAttecked?
 		if (board.getCatchable(board.getNextTurn(), KingX, KingY)) {
 			return true;
@@ -38,52 +41,65 @@ public class Checker implements ConstDef {
 		}
 		// find king position
 		int KingX = 0, KingY = 0;
+		Position KingPos = null;
 
-		for (int X = 0; X < board.boardSize; X++) {
+		LABEL: for (int X = 0; X < board.boardSize; X++) {
 			for (int Y = 0; Y < board.boardSize; Y++) {
 				if (board.getPiece(X, Y).getName() == KING && board.getPiece(X, Y).getColor() == board.getTurn()) {
 					KingX = X;
 					KingY = Y;
+					KingPos = new Position(X, Y);
+					break LABEL;
 				}
 			}
 		}
-		// done
+
 		// king cannot move
-		for (int X = -1; X < 2 && (KingX+X < board.boardSize && 0 <= KingX+X); X++) {
-			for (int Y = -1; Y < 2 && (KingY+Y < board.boardSize && 0 <= KingY+Y); Y++) {
-				if(X != 0 && Y != 0) {
-					if(board.getPiece(KingX+X,KingY+Y) == null) {
-						if(( !board.getCatchable(board.getNextTurn(), KingX+X, KingY+Y) || !board.getCatchable(board.getPrevTurn(), KingX+X, KingY+Y))) {
-							return false;
-						}
-					}else if ((board.getPiece(KingX+X, KingY+Y).getColor() == board.getPrevTurn() || board.getPiece(KingX+X, KingY+Y).getColor() == board.getNextTurn())) {
-						return false;
-					}
-				}
+		ArrayList<Position> KingMovement = board.getPiece(KingPos).getMovement(board, KingPos);
+		for (int i = 0; i < KingMovement.size(); i++) {
+			if (board.getCatchable(board.getNextTurn(), KingMovement.get(i).getX(), KingMovement.get(i).getY())) {
+			}
+			else {
+				return false;
 			}
 		}
-		
+
 		// there is no legal move
-		
+
 		for (int X = 0; X < board.boardSize; X++) {
 			for (int Y = 0; Y < board.boardSize; Y++) {
-				if (board.getCatchable(board.getTurn(), X, Y))
-					return false;
+				Piece nowPiece = board.getPiece(X, Y);
+				if(nowPiece.getColor() == board.getTurn()) {
+					Position nowPos = new Position(X, Y);
+					ArrayList<Position> nowPieceMove = nowPiece.getMovement(board, nowPos);
+					for(int i= 0; i<nowPieceMove.size(); i++) {
+						if(!board.isIllegalMove(nowPos, nowPieceMove.get(i))){
+							return false;
+						}
+					}
+				}
 			}
 		}
 		return true;
 	}
 
-
 	public boolean isStaleMate() {
 		if (isChecked()) {
 			return false;
 		}
-		//no legal move
+		// no legal move
 		for (int X = 0; X < board.boardSize; X++) {
 			for (int Y = 0; Y < board.boardSize; Y++) {
-				if ((board.getCatchable(board.getTurn(), X, Y)))
-					return false;
+				Piece nowPiece = board.getPiece(X, Y);
+				if(nowPiece.getColor() == board.getTurn()) {
+					Position nowPos = new Position(X, Y);
+					ArrayList<Position> nowPieceMove = nowPiece.getMovement(board, nowPos);
+					for(int i= 0; i<nowPieceMove.size(); i++) {
+						if(!board.isIllegalMove(nowPos, nowPieceMove.get(i))){
+							return false;
+						}
+					}
+				}
 			}
 		}
 		return true;
